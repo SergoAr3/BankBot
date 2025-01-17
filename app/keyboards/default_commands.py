e22a9c9fef08d24c4be257721ef61e1c41ad2eb9
Серёжа <ser06070203@gmail.com>
2025-01-17 20:41:59 +0300

    add keyboards and commands

---
+++ app/keyboards/default_commands.py
@@ -0,0 +1,16 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault, BotCommandScopeAllGroupChats
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="balance", description="Узнать баланс"),
+        BotCommand(command="transfer", description="Перевести монеты"),
+        BotCommand(command="buy_cat", description="Купить котика"),
+        BotCommand(command="credit", description="Взять кредит"),
+        BotCommand(command="credit_info", description="Мой кредит"),
+        BotCommand(command="deposit", description="Открыть вклад"),
+        BotCommand(command="deposit_info", description="Мои вклады"),
+        BotCommand(command="subscriptions", description="Подписки на каналы"),
+    ]
+    await bot.set_my_commands(commands, scope=BotCommandScopeDefault())
---
+++ app/keyboards/inline_keyboards.py
@@ -0,0 +1,98 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def get_menu_button() -> InlineKeyboardButton:
+    menu_button = InlineKeyboardButton(text='Главное меню 📖', callback_data='menu')
+
+    return menu_button
+
+
+async def get_back_menu_button() -> InlineKeyboardButton:
+    back_menu_kb = InlineKeyboardButton(text='Назад ⏪', callback_data='back')
+
+    return back_menu_kb
+
+
+async def get_one_button_kb(button: InlineKeyboardButton) -> InlineKeyboardMarkup:
+    one_button_kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                button
+            ],
+
+        ],
+        resize_keyboard=True
+    )
+
+    return one_button_kb
+
+
+async def get_menu_kb() -> InlineKeyboardMarkup:
+    menu_kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text='Мой баланс 💸‍', callback_data='balance')
+            ],
+            [
+                InlineKeyboardButton(text='Перевести монеты 🔄‍', callback_data='transfer')
+            ],
+            [
+                InlineKeyboardButton(text='Купить котика 😸‍', callback_data='buy_cat')
+            ],
+            [
+                InlineKeyboardButton(text='Кредит 💵', callback_data='credit')
+            ],
+            [
+                InlineKeyboardButton(text='Вклад 📈', callback_data='deposit')
+            ],
+            [
+                InlineKeyboardButton(text='Подписки на каналы ✅', callback_data='subscriptions')
+            ],
+
+        ],
+        resize_keyboard=True
+    )
+
+    return menu_kb
+
+
+def get_credit_kb():
+    back_menu_button = get_back_menu_button()
+    credit_kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text='Взять кредит 💵', callback_data='take_credit')
+            ],
+            [
+                InlineKeyboardButton(text='Мои кредиты 🔎', callback_data='credit_info')
+            ],
+            [
+                back_menu_button
+            ],
+
+        ],
+        resize_keyboard=True
+    )
+
+    return credit_kb
+
+
+def get_deposit_kb():
+    back_menu_button = get_back_menu_button()
+    deposit_kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text='Открыть вклад 📈', callback_data='take_credit')
+            ],
+            [
+                InlineKeyboardButton(text='Мои вклады 🔎', callback_data='credit_info')
+            ],
+            [
+                back_menu_button
+            ],
+
+        ],
+        resize_keyboard=True
+    )
+
+    return deposit_kb
