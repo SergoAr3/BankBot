a64b4d2a486d8ba1b5e3ac13f06aca67379039a4
Серёжа <ser06070203@gmail.com>
2025-01-21 02:41:24 +0300

    add "Купить котика" button functional

--- app/utils/states.py
+++ app/utils/states.py
@@ -4,23 +4,3 @@ class Transfer(StatesGroup):
     amount = State()
     username = State()
     confirm = State()
-
-# class Start(StatesGroup):
-#     start = State()
-#
-#
-# class SetReminder(StatesGroup):
-#     set_time = State()
-#
-#
-# class Choice(StatesGroup):
-#     choice_first = State()
-#     choice_second = State()
-#
-#
-# class Invite(StatesGroup):
-#     friend_nickname = State()
-#
-#
-# class Payment(StatesGroup):
-#     payment = State()
